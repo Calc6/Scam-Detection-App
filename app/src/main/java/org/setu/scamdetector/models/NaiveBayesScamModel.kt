7a5b8ad9afc3b1e2c20aaa0961cd819ab21cee04
Calum Cullen <Calum.cullen66@gmail.com>
2026-02-20 22:48:39 +0000

    Add load guard + logs

--- app/src/main/java/org/setu/scamdetector/models/NaiveBayesScamModel.kt
+++ app/src/main/java/org/setu/scamdetector/models/NaiveBayesScamModel.kt
@@ -1,2 +1,19 @@
-val nb = NaiveBayesScamModel(this)
-nb.loadIfNeeded()
+package org.setu.scamdetector.models
+
+import android.content.Context
+import android.util.Log
+
+class NaiveBayesScamModel(private val context: Context) {
+
+    private var vocab: Map<String, Int>? = null
+    private var classLogPrior: FloatArray? = null
+    private var featureLogProb: Array<FloatArray>? = null
+
+    fun loadIfNeeded() {
+        if (vocab != null && classLogPrior != null && featureLogProb != null) {
+            Log.d("NBModel", "Already loaded")
+            return
+        }
+        Log.d("NBModel", "Not loaded yet")
+    }
+}
