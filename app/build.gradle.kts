687e419c12d05594ed9e21bdf1c0e0340605ac3e
Calum Cullen <Calum.cullen66@gmail.com>
2026-02-20 18:56:52 +0000

    Added depends for JSON + NPZ reading

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -48,6 +48,8 @@ dependencies {
     implementation(libs.androidx.constraintlayout)
     implementation(libs.timber)
     implementation("com.google.mlkit:text-recognition:16.0.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.3")
+    implementation("com.github.kushilsahu:android-npz:1.0.2")
 testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
